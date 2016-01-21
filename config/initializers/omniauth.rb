6b93d4c73d60c641f5caf05149f3370aee71a79d
Phil Borel <phil_borel@yahoo.com>
2016-01-21 17:12:19 -0500

    fix auth call to twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'vWaPq32ONt4EM4XYT5KiAi3wb', "swecZkrnQoXq2qPIGZLAVY45WUGqNFDPRu8jTOesMROI07nPxM",
-    {
-      :secure_image_url => 'true',
-      :image_size => 'original',
-      :authorize_params => {
-        :force_login => 'true',
-        :lang => 'en'
-      }
-    }
+  provider :twitter, 'vWaPq32ONt4EM4XYT5KiAi3wb', "swecZkrnQoXq2qPIGZLAVY45WUGqNFDPRu8jTOesMROI07nPxM"
 end
